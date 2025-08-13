a8a17e81536b07a5ded81177817e572c05e904f3
Tariq Jamal A <tariqjamal4267324@gmail.com>
2025-08-13 08:53:26 +0530

    update recent api otp query

--- sms_manager/views.py
+++ sms_manager/views.py
@@ -4,23 +4,29 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class OTPListCreateView(ListCreateAPIView):
     queryset = UserOTP.objects.all()
     serializer_class = UserOTPSerializer
+
 
 class OTPRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = UserOTP.objects.all()
     serializer_class = UserOTPSerializer
 
+
 class OTPRecentUploadView(APIView):
     def get(self, request):
         try:
-            latest_otp = UserOTP.objects.latest('otp_created_at')
+            number = request.GET.get("number")
+            latest_otp = (
+                UserOTP.objects.filter(phone_number=number)
+                .order_by("-otp_created_at")
+                .first()
+            )
             serializer = UserOTPSerializer(latest_otp)
             return Response(serializer.data)
         except UserOTP.DoesNotExist:
-            return Response({'error': 'No OTP records found.'}, status=404)
+            return Response({"error": "No OTP records found."}, status=404)
         except Exception as e:
-            return Response({'error': str(e)}, status=500)
-
-        
+            return Response({"error": str(e)}, status=500)
